d69ce0a15ba36f24923a14725e7325385ea1f936
Paavan Chopra <paavan012chopra@gmail.com>
2018-07-08 11:24:23 -0400

    finalize

--- src/main/java/com/giftcard/price/PriceApplication.java
+++ src/main/java/com/giftcard/price/PriceApplication.java
@@ -3,16 +3,11 @@ package com.giftcard.price;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import com.giftcard.price.manager.impl.GiftCardManagerImpl;
 
-//@SpringBootApplication
 public class PriceApplication {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		//SpringApplication.run(PriceApplication.class, args);
 		GiftCardManagerImpl gi = new GiftCardManagerImpl();
 		System.out.println(gi.findPair(new FileInputStream(args[0]), Integer.parseInt(args[1])));
 	}
